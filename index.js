fc527baf5a0dc2b683e1dc46819e61bbf938752c
kamron-dev <109903041+kamron-dev@users.noreply.github.com>
2022-12-15 20:42:18 +0500

    update to index.js
    Added the playRound function that prompts for user input and alerts the winner, some weird alert of 'undefined' comes up need to investigate

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const items = ['Rock', 'Paper', 'Scissors']
+const items = ['rock', 'paper', 'scissors']
 
 // Function that randomly assigns either rock paper or scissors for the computer
 function getComputerChoice(items) {
     // this line returns random from the items array by passing a random number (* 3 gives a random number from 0 to 2)
     return items[Math.floor(Math.random() * 3)]
 }
-console.log(getComputerChoice(items))
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+    } else  if (playerSelection === 'paper' && computerSelection === 'rock'){
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+    } else {
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`)
+    }
+  }
+   
+  const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase();
+  const computerSelection = getComputerChoice(items);
+  alert(playRound(playerSelection, computerSelection));
