04a4cfcc15da29383b1b19a6ef5c0de49d0ecbcc
kamron-dev <109903041+kamron-dev@users.noreply.github.com>
2022-12-16 17:35:53 +0500

    Update to index.js + score variables
    added score variables

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const items = ['rock', 'paper', 'scissors']
+let playerScore = 0;
+let computerScore = 0;
 
 // Function that randomly assigns either rock paper or scissors for the computer
 function getComputerChoice(items) {
     // this line returns random from the items array by passing a random number (* 3 gives a random number from 0 to 2)
-    return items[Math.floor(Math.random() * 3)]
+    return items[Math.floor(Math.random() * items.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1; 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1;
     } else  if (playerSelection === 'paper' && computerSelection === 'rock'){
         alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1;
     } else if (playerSelection === computerSelection) {
         alert(`Tie!`)
+        return;
     } else {
         alert(`You lose! ${computerSelection} beats ${playerSelection}`)
+        return computerScore += 1;
     }
   }
    
   const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase();
   const computerSelection = getComputerChoice(items);
   playRound(playerSelection, computerSelection);
+  alert(playerScore);
+  alert(computerScore);
