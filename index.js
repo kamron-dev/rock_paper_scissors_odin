5177ace16ad367a73a1410e8a7290cd5aa832395
kamron-dev <109903041+kamron-dev@users.noreply.github.com>
2022-12-16 16:43:24 +0500

    Update to index.js
    Working game function that plays 5 rounds

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const items = ['rock', 'paper', 'scissors']
 
+let playerScore = 0;
+let computerScore = 0;
+
 // Function that randomly assigns either rock paper or scissors for the computer
 function getComputerChoice(items) {
-    // this line returns random from the items array by passing a random number (* 3 gives a random number from 0 to 2)
-    return items[Math.floor(Math.random() * 3)]
+    // this line returns random from the items array by passing a random number (* items.length gives a random number from 0 to 3)
+    return items[Math.floor(Math.random() * items.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
+<<<<<<< Updated upstream
         return `You won! ${playerSelection} beats ${computerSelection}!`
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         return `You won! ${playerSelection} beats ${computerSelection}!`
     } else  if (playerSelection === 'paper' && computerSelection === 'rock'){
         return `You won! ${playerSelection} beats ${computerSelection}!`
+=======
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1;
+
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1;
+
+    } else  if (playerSelection === 'paper' && computerSelection === 'rock'){
+        alert(`You won! ${playerSelection} beats ${computerSelection}!`)
+        return playerScore += 1;
+
+>>>>>>> Stashed changes
     } else if (playerSelection === computerSelection) {
        return `Tie!`
     } else {
+<<<<<<< Updated upstream
         return `You lose! ${computerSelection} beats ${playerSelection}`
     }
   }
@@ -35,3 +53,27 @@ function playRound(playerSelection, computerSelection) {
     }
     
   }
+=======
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`)
+        return computerScore += 1;
+    }
+  }
+   
+  //const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase();
+//   const computerSelection = getComputerChoice(items);
+  //playRound(playerSelection, computerSelection);
+  
+  function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose rock, paper or scissors!").toLowerCase();
+        const computerSelection = getComputerChoice(items);
+        playRound(playerSelection, computerSelection)
+    }
+    if (playerScore > computerScore) {
+        alert(`You won! Your score is ${playerScore}`)
+    } else if ( playerScore < computerScore) {
+        alert(`You lost! Computer's score is ${computerScore}`)
+    }
+  }
+  game()
+>>>>>>> Stashed changes
